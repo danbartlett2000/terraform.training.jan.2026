2d9c44a1d54b9e84e57ba324276ce1872edab4e5
crem72 <curtisreynoldsphoto@gmail.com>
2026-01-08 21:26:24 +0000

    updated

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,12 @@
 resource "aws_instance" "example" {
+  for_each = var.ec2_instances
   ami           = var.db-ami
-  instance_type = var.db-vm-size
-  count         = var.novm
+  instance_type = each.value
+  #count = var.novm
+  # attaching security group to ec2 vm 
+  vpc_security_group_ids = [ aws_security_group.allow_tls.id ]
 
   tags = {
-    Name = "$(var.db-vm-name)-$(count.index)"
+    Name = "$(var.db-vm-name)-${each.key}"
   }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,7 @@
-output "vm_public_ip" {
-  value = aws_instance.example.*.public_ip
-}
+#output "db-vm-publicIP" {
+#  value = aws_instance.example.*.public_ip
+#}
 
-output "vm_private_ip" {
-  value = aws_instance.example.*.private_ip
-}
-
-output "vm_ami" {
-  value = aws_instance.example.*.ami
-}
-
-output "vm_ID" {
-  value = [ for i in aws_instance.example: i.id]
-}
-
-output "security_group_names" {
-  value = aws_instance.example.*.security_groups
-}
+#output "db-vm-id" {
+#  value = [ for i in aws_instance.example: i.id ]
+#}
